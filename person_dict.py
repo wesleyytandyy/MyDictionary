396fa07cd3c8f98de3ec9e6347b6d9a1d2fe9b7b
Wesley Tandy <Wesley_Tandy1@baylor.edu>
2021-02-01 13:34:32 -0600

    Added to files

---
+++ person_dict.py
@@ -0,0 +1,22 @@
+person = {}
+person['fname'] = 'Joe'
+person['lname'] = 'Fonebone' 
+person['age'] = 51
+person['spouse'] = 'Edna'
+person['children'] = ['Ralph', 'Betty', 'Joey']
+person['pets'] = {'dog': 'Fido', 'cat': 'Sox'} 
+
+
+
+print(person['children'])
+
+
+print(person['children'][1])
+
+
+print(person['pets']['cat'])
+
+
+
+
+                      
